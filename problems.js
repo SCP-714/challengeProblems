280eaefa3cbcfb79afd4cdd3f0a8716f2cf6a8c1
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-03-23 13:10:24 -0500

    longest palendrome

--- problems.js
+++ problems.js
@@ -26,3 +26,25 @@ var isPalindrome = function(x) {
         return false;
     };
  };
+
+
+
+
+ var longestPalindrome = function(s) {
+    let str="";
+
+    for(let i=0;i<s.length;i++){
+        for(let j=0;j<2;j++){
+            let left=i;
+            let right=left+j;
+            while(s[left]&&s[left]===s[right]){
+                left--;
+                right++;
+            }
+            if(right-left-1>str.length){
+                str=s.slice(left+1,right);
+            }
+        }
+    }
+    return str;
+};
